f4f986c4de318a494f01ec86a7c6cbb7ad6ce748
Russ Butler <russ.butler@arm.com>
2016-02-26 09:45:14 -0600

    Turn off HID endpoint to allow more USB devices
    The HID endpoint reserves a fixed amount of bandwidth when it is
    turned on.  This limits the number of boards that can be connected
    to a single USB hub to between 9 and 19 boards.  This patch turns
    off the HID endpoint so a large number of devices can be connected
    to USB.

--- source/daplink/interface/main.c
+++ source/daplink/interface/main.c
@@ -70,7 +70,6 @@ static main_led_state_t msc_led_state = MAIN_LED_FLASH;
 main_usb_connect_t usb_state;
 
 static U64 stk_timer_30_task[TIMER_TASK_30_STACK/8];
-static U64 stk_dap_task[DAP_TASK_STACK/8];
 static U64 stk_serial_task[SERIAL_TASK_STACK/8];
 static U64 stk_main_task[MAIN_TASK_STACK/8];
 
@@ -225,7 +224,6 @@ __task void serial_process()
     }
 }
 
-extern __task void hid_process(void);
 __attribute__((weak)) void prerun_target_config(void){}
 
 __task void main_task(void)
@@ -337,7 +335,6 @@ __task void main_task(void)
                 case USB_CHECK_CONNECTED:
                     if(usbd_configured()) {
                         if (!thread_started) {
-                            os_tsk_create_user(hid_process, DAP_TASK_PRIORITY, (void *)stk_dap_task, DAP_TASK_STACK);
                             serial_task_id = os_tsk_create_user(serial_process, SERIAL_TASK_PRIORITY, (void *)stk_serial_task, SERIAL_TASK_STACK);
                             thread_started = 1;
                         }
